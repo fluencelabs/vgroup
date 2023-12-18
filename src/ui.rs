5216d8246fe4539ccbf4e57150e8b8b89521fabd
folex <0xdxdy@gmail.com>
2023-12-18 00:21:42 -0300

    chore: trying to set up an event bus

--- src/ui.rs
+++ src/ui.rs
@@ -2,14 +2,20 @@
 
 use std::fmt::format;
 
-use cursive::{Cursive, CursiveRunnable, Printer};
 use cursive::event::Event;
 use cursive::logger::Record;
-use cursive::reexports::log::{Level, log};
+use cursive::reexports::log::{log, Level};
 use cursive::theme::{BaseColor, Color, Theme};
 use cursive::traits::*;
 use cursive::view::SizeConstraint;
-use cursive::views::{Button, DebugView, Dialog, DummyView, EditView, LinearLayout, ListView, Panel, ResizedView, SelectView, SliderView, TextArea, TextContent, TextView, ThemedView};
+use cursive::views::{
+    Button, DebugView, Dialog, DummyView, EditView, LinearLayout, ListView, Panel, ResizedView,
+    SelectView, SliderView, TextArea, TextContent, TextView, ThemedView,
+};
+use cursive::{Cursive, CursiveRunnable, Printer};
+use futures::StreamExt;
+use tokio::runtime::Runtime;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 
 // use cursive_core::traits::Resizable;
 
@@ -20,9 +26,7 @@ fn worker_item(path: &str, percent: usize) -> LinearLayout {
     let mut percent_label = TextView::new_with_content(percent_content.clone());
 
     let mut slider = SliderView::horizontal(100)
-        .on_change(move |s, percent| {
-            percent_content.set_content(format!("  {percent}%  "))
-        });
+        .on_change(move |s, percent| percent_content.set_content(format!("  {percent}%  ")));
 
     slider.set_value(percent);
     let slider = slider.max_width(20);
@@ -33,6 +37,68 @@ fn worker_item(path: &str, percent: usize) -> LinearLayout {
         .child(percent_label);
 
     layout
+}
+
+#[derive(Debug, Clone)]
+pub enum ControlEvent {
+    WorkersCreated(Vec<String>),
+}
+
+#[derive(Debug, Clone)]
+pub enum CGroupRef {
+    Tokio,
+    Worker(usize),
+}
+
+#[derive(Debug, Clone)]
+pub enum UIEvent {
+    CPULimitChanged(CGroupRef, usize),
+}
+
+pub struct UIRef {
+    sender: UnboundedSender<ControlEvent>,
+    receiver: UnboundedReceiver<UIEvent>,
+}
+
+pub struct ControlRef {
+    sender: UnboundedSender<UIEvent>,
+    receiver: UnboundedReceiver<ControlEvent>,
+}
+
+pub struct UIRuntime {
+    ui_ref: UIRef,
+    control_ref: ControlRef,
+    runtime: Runtime,
+}
+
+pub fn runtime() -> UIRuntime {
+    use tokio::runtime::Builder;
+
+    let (to_ui, from_control) = tokio::sync::mpsc::unbounded_channel();
+    let (to_control, from_ui) = tokio::sync::mpsc::unbounded_channel();
+    let runtime = Builder::new_multi_thread()
+        .worker_threads(1)
+        .max_blocking_threads(1)
+        .build()
+        .expect("build UI tokio runtime");
+
+    // runtime.spawn(async move {
+    //     if let Ok(command) = from_control.try_recv() {
+    //
+    //     }
+    // });
+
+    UIRuntime {
+        ui_ref: UIRef {
+            sender: to_ui,
+            receiver: from_ui,
+        },
+        control_ref: ControlRef {
+            sender: to_control,
+            receiver: from_control,
+        },
+        runtime,
+    }
 }
 
 pub fn show() {
@@ -96,16 +162,16 @@ fn add_name(s: &mut Cursive) {
                 .with_name("name")
                 .fixed_width(10),
         )
-            .title("Enter a new name")
-            .button("Ok", |s| {
-                let name = s
-                    .call_on_name("name", |view: &mut EditView| view.get_content())
-                    .unwrap();
-                ok(s, &name);
-            })
-            .button("Cancel", |s| {
-                s.pop_layer();
-            }),
+        .title("Enter a new name")
+        .button("Ok", |s| {
+            let name = s
+                .call_on_name("name", |view: &mut EditView| view.get_content())
+                .unwrap();
+            ok(s, &name);
+        })
+        .button("Cancel", |s| {
+            s.pop_layer();
+        }),
     );
 }
 
