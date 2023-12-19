b9462bfa93066358691d5583be84015c4f82cfce
folex <0xdxdy@gmail.com>
2023-12-19 00:04:40 -0300

    revert: local history to before workers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate core;
-
 use std::io::Write;
 use std::ops::{Div, Mul};
 use std::sync::Arc;
@@ -19,48 +17,49 @@ use tokio::runtime::Runtime;
 use tokio::task::yield_now;
 
 const BACKSPACE: char = 8u8 as char;
-const WORKERS: usize = 3;
 
 // #[tokio::main]
 fn main() {
+    // let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+
     // Create nox/tokio cgroup hierarchy
-    let cgroups = make_cgroup(WORKERS);
+    let tokio = make_cgroup();
+    let tokio_ctrl: &CpuController = tokio.controller_of().unwrap();
 
     // Retrieve 'nox' cgroup, it's the parent cgroup of the tokio cgroup
     println!("\n# nox:");
-    print_group(&cgroups.nox);
+    let nox = tokio.parent_control_group();
+    print_group(&nox);
 
     // Print tokio group before and after cpu limit
     println!("# tokio:");
-    print_group(&cgroups.tokio);
+    set_cpu_limit(&tokio_ctrl, 5);
+    print_group(&tokio);
 
     // Move current PID to the nox group
     let pid = libc::pid_t::from(nix::unistd::getpid()) as u64;
-    cgroups.nox.add_task_by_tgid(CgroupPid::from(pid)).unwrap();
+    nox.add_task_by_tgid(CgroupPid::from(pid)).unwrap();
 
-    let mut procs = cgroups.nox.procs().into_iter();
+    let mut procs = nox.procs().into_iter();
     // Verify that the task is indeed in the x control group
     assert_eq!(procs.next(), Some(CgroupPid::from(pid)));
     assert_eq!(procs.next(), None);
 
     // create tokio runtime with N threads
-    let threads_num = read_threads();
-    let threads = create_threads(threads_num);
+    let threads = read_threads();
+    let threads = create_threads(threads);
     println!("created threads {:?}", threads.ids);
 
-    let group_size = threads_num / WORKERS;
-    let thread_ids = threads.ids.chunks(group_size);
-    for (i, ids) in thread_ids.enumerate() {
-        // Move groups of threads to 'tokio/worker_$i' cgroup
-        assign_threads(&cgroups.workers[i], ids);
-    }
+    // Move these threads to 'tokio' cgroup
+    assign_threads(&tokio, &threads.ids);
 
-    read_limit(threads, cgroups);
+    read_limit(threads, &tokio);
 }
 
-fn read_limit(threads: Threads, groups: CGroups) {
+fn read_limit(threads: Threads, group: &Cgroup) {
     loop {
-        print!("<worker idx> <limit %> | tokio <limit %> | stop: _{BACKSPACE}");
+        print!("Limit (% | stop): _{BACKSPACE}");
         std::io::stdout().flush().expect("flush");
 
         let mut action = String::new();
@@ -71,30 +70,13 @@ fn read_limit(threads: Threads, groups: CGroups) {
             break send_stop(threads);
         }
 
-        let action = action.split(" ").collect::<Vec<_>>();
-        if let &[worker, limit] = action.as_slice() {
-            if let Ok(limit) = limit.parse::<u64>() {
-                // Set CPU limit for the whole 'nox/tokio' cgroup
-                if worker == "tokio" {
-                    set_cpu_limit(&groups.tokio, limit);
-                    print_group(&groups.tokio);
-                    continue;
-                }
-
-                // Set CPU limit for a single worker
-                if let Ok(worker) = worker.parse::<usize>() {
-                    if worker < groups.workers.len() {
-                        let group = &groups.workers[worker];
-                        set_cpu_limit(group, limit as u64);
-                        print_group(group);
-                        continue;
-                    }
-                }
-            }
+        if let Ok(limit) = action.parse::<u64>() {
+            let ctrl = group.controller_of().expect("controller");
+            set_cpu_limit(ctrl, limit);
+            print_group(group);
+        } else {
+            println!("invalid input: {:?}", action);
         }
-
-        // None of the 'continues' were hit
-        println!("invalid input: {:?}", action);
     }
 }
 
@@ -123,7 +105,7 @@ fn assign_threads(group: &Cgroup, thread_ids: &[u64]) {
     // Move these threads to 'tokio' cgroup
     for tid in thread_ids {
         if let Err(err) = group.add_task(CgroupPid::from(*tid)) {
-            println!("error add thread {tid} to '{}' cgroup: {:?}", group.path(), err);
+            println!("error add thread {tid} to 'tokio' cgroup: {:?}", err);
         }
     }
     let tasks = group
@@ -211,7 +193,7 @@ fn create_threads(n: usize) -> Threads {
 /// Set cpu controller period to 10 ms by default. Why not?
 const PERIOD_MS: u64 = 10;
 
-fn set_cpu_limit(group: &Cgroup, percent: u64) {
+fn set_cpu_limit(ctrl: &CpuController, percent: u64) {
     let period = PERIOD_MS;
     let quota = (period as f64 * percent as f64) / 100f64;
     let quota = quota.ceil() as u64;
@@ -219,21 +201,14 @@ fn set_cpu_limit(group: &Cgroup, percent: u64) {
     let period = Some(Duration::from_millis(period).as_micros() as u64);
     let quota = Some(Duration::from_millis(quota).as_micros() as i64);
 
-    let ctrl: &CpuController = group.controller_of().expect("set_cpu_limit: get group controller");
     ctrl.set_cfs_quota_and_period(quota, period)
         .expect(&format!("set CPU quota to {}", ctrl.path().display()));
 }
 
-struct CGroups {
-    nox: Cgroup,
-    tokio: Cgroup,
-    workers: Vec<Cgroup>,
-}
-
-fn make_cgroup(workers: usize) -> CGroups {
+fn make_cgroup() -> Cgroup {
     use cgroups_rs::hierarchies::auto;
 
-    let nox = Cgroup::new(auto(), String::from("nox")).unwrap();
+    let _cg = Cgroup::new(auto(), String::from("nox")).unwrap();
 
     let tokio = Cgroup::new_with_specified_controllers(
         auto(),
@@ -242,17 +217,10 @@ fn make_cgroup(workers: usize) -> CGroups {
     )
         .expect("create tokio cg");
 
+    // Set cgroup type of the sub-control group is thread mode.
+    tokio.set_cgroup_type(CGROUP_MODE_THREADED).unwrap();
 
-    let workers = (0..workers).map(|i| {
-        let path = format!("nox/tokio/worker_{}", i);
-        let controllers = vec!["cpuset", "cpu"].into_iter().map(|g| g.to_string()).collect();
-        let group = Cgroup::new_with_specified_controllers(auto(), path, Some(controllers))?;
-        // Set cgroup type of the sub-control group is thread mode.
-        group.set_cgroup_type(CGROUP_MODE_THREADED).unwrap();
-        Ok(group)
-    }).collect::<error::Result<_>>().expect("create worker cgroups");
-
-    CGroups { nox, tokio, workers }
+    tokio
 }
 
 fn print_group(group: &Cgroup) {
@@ -290,7 +258,7 @@ fn print_controller(ctrl: &CpuController) {
     let typ = ctrl.get_cgroup_type().unwrap();
     let path = ctrl.path().display();
 
-    println!("Path:   {: <35}Type: {: <10}", path, typ);
+    println!("Path:   {: <25}Type: {: <10}", path, typ);
     let percent = (quota as f64).div(period as f64).mul(100f64).to_string();
     let percent = format!("{}%", percent);
     let limit = if quota > 0 {
@@ -298,5 +266,5 @@ fn print_controller(ctrl: &CpuController) {
     } else {
         "no limit"
     };
-    println!("Period: {: <35}Quota: {}\t{}\n", period, quota, limit);
+    println!("Period: {: <25}Quota: {}\t{}\n", period, quota, limit);
 }
